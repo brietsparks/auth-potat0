82435d269f237d9e53b4051579e9bdda51ee4c19
Briet Sparks <bws46@cornell.edu>
2018-01-05 23:31:33 -0600

    feat: Fix types

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,4 @@
+export type Action = { type: string; payload: any }
+export type Map<T> = { [key: string]: any }
 export type State<T> = Map<T>
 export type Reducer<T> = (state: State<T>, action: Action) => State<T>
